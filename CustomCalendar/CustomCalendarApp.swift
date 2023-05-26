1b02af1d7203773037c99792625c2574464581a5
Ulyanenkov Yaroslav <vashworld01@gmail.com>
2023-05-26 18:17:13 +0300

    Initial Commit

---
+++ CustomCalendar/CustomCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCalendarApp.swift
+//  CustomCalendar
+//
+//  Created by Ярослав Ульяненков on 26.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct CustomCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
